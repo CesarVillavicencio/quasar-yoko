788832b9e5e613fd1dc06064aa647ef06f417ad9
Cesar Villavicencio <boucin@hotmail.com>
2020-12-29 15:59:23 -0700

    cambio de URL api

--- src/store/datos/actions.js
+++ src/store/datos/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function getDatos ({commit}, ruta) {
 	// alert(ruta);
-  return axios.get('https://y.codigorex.com/api/v1/r/'+ruta)
+  return axios.get('https://panel.yokoportal.com/api/v1/r/'+ruta)
     .then(response => {
     	// alert("ahi viene ");
       commit('setInformacion', response.data)
